e94afb770c3b92da1528efbbbb514d378c5e3b78
Michael Jeremy Treacy <MichaelJeremyTreacy@outlook.com>
2021-08-30 11:36:36 -0400

    Add example queries

---
+++ SQLQuery1.sql
@@ -0,0 +1,21 @@
+﻿SELECT * FROM Book
+
+SELECT * FROM Genre
+
+SELECT a.Name FROM Genre a
+INNER JOIN BookGenre b ON a.Id = b.GenreId
+WHERE b.BookId = 1;
+
+SELECT a.Name FROM Genre a
+INNER JOIN BookGenre b ON a.Id = b.GenreId
+WHERE b.BookId = 11;
+
+UPDATE Book
+SET Name = 'The Holy Bible'
+WHERE Id = 1;
+
+INSERT INTO Book
+VALUES ('The Cancer Code', 'Dr. Jason Fung', 360, 'A look at the history of cancer.');
+
+DELETE FROM Book
+WHERE Id = 12;
